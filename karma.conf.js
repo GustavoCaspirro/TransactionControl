30f3fe341e54cc9a2507fdc4b6d1d9d25f348aa1
Gustavo Caspirro <gustavo.caspirro-ext@viavarejo.com.br>
2020-05-12 16:07:33 -0300

    corrigindo captured browser mensagem de build do travis

--- karma.conf.js
+++ karma.conf.js
@@ -20,6 +20,16 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222',
+        ],
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
