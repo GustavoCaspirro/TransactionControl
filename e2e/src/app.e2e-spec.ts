f9667c349073d3bc1cb9062322f8be4ccad190fe
Gustavo Caspirro <gustavo.caspirro-ext@viavarejo.com.br>
2020-05-12 11:13:12 -0300

    ajustes para ver se build travis funciona

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to FinancialControl!');
+    expect(page.getTitleText()).toEqual('Welcome to TransactionControl!');
   });
 
   afterEach(async () => {
